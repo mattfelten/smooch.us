ba6be6ad90dd51eba7f6b069ed34e469a0b721b3
Matt Felten <matt.felten@dreamhost.com>
2015-07-14 02:05:30 -0700

    Upload first pass at website

---
+++ assets/js/footer.js
@@ -0,0 +1,26 @@
+$(document).ready(function(){
+	function parallaxFooterImage() {
+		var $window = $(window),
+			$footer = $('.site-footer'),
+			windowBottom = $(window).scrollTop() + $(window).height(),
+			footerTop = $footer.offset().top + 185;
+
+		// die if footer isn't in view
+		if (windowBottom < footerTop) return;
+
+		var amountOfPixelsShown = windowBottom - footerTop;
+		var decimalOfPixelsShown = amountOfPixelsShown / ($footer.outerHeight() - 185);
+
+		// Needs to be from 600% when off screen to 100% when at bottom
+		var yPos = ((1 - decimalOfPixelsShown) * 500) + 100;
+
+		var coords = '50% '+ yPos + '%';
+		$('.site-footer').css({ backgroundPosition: coords });
+	}
+
+	parallaxFooterImage();
+
+	$(window).scroll(function() {
+		parallaxFooterImage();
+	});
+});
